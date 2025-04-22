4d4e96481fae9e93093372b3d5a449dcdfa4339f
LuizBoldrini <luizboldriniej@gmail.com>
2025-04-22 00:40:57 -0400

    Alterado validação para Conjuntos e consumido rota das tarefas

--- TarefasToDo/Service/ServiceAPI.cs
+++ TarefasToDo/Service/ServiceAPI.cs
@@ -105,6 +105,7 @@ namespace TarefasToDo.Service
             }
         }
 
+        
         public async Task<List<ConjuntoLista>> ListaConjuntosUsuario(int usuarioLogadoId)
         {
             try
@@ -202,7 +203,7 @@ namespace TarefasToDo.Service
                     var erro = await response.Content.ReadAsByteArrayAsync();
                     var utf8String = Encoding.UTF8.GetString(erro);
 
-                    string mensagemErro = "Erro ao excluir conjunto.";
+                    string mensagemErro = "Erro ao editar conjunto.";
 
                     try
                     {
@@ -237,7 +238,6 @@ namespace TarefasToDo.Service
                 throw;
             }
         }
-
 
         public async Task<bool> DeletarConjunto(int conjuntoId)
         {
@@ -276,18 +276,214 @@ namespace TarefasToDo.Service
             }
         }
 
-        public async Task<List<Tarefa>> ListaTarefasConjunto(int conjuntoId)
-        {
-            var response = await _http.GetAsync($"/conjunto/tarefas/{conjuntoId}");
-            response.EnsureSuccessStatusCode();
-            var tarefa = await response.Content.ReadFromJsonAsync<List<Tarefa>>();
-            
-            if(tarefa == null)
-            {
-                throw new InvalidOperationException("A resposta da API retornou um objeto nulo.");
-            }
 
-            return tarefa;
+        public async Task<List<TarefaLista>> ListaTarefasConjunto(int conjuntoId)
+        {
+            try
+            {
+                var response = await _http.GetAsync($"{GlobalSettings.Instance.Tarefa}/{conjuntoId}");
+
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return new List<TarefaLista>();
+                }
+                if (!response.IsSuccessStatusCode)
+                {
+                    var erro = await response.Content.ReadAsByteArrayAsync();
+                    var utf8String = Encoding.UTF8.GetString(erro);
+
+                    var json = JsonDocument.Parse(utf8String);
+                    var mensagem = json.RootElement.GetProperty("message").GetString();
+
+                    throw new Exception(mensagem);
+                }
+                var tarefa = await response.Content.ReadFromJsonAsync<List<TarefaLista>>();
+
+                if (tarefa == null)
+                {
+                    throw new InvalidOperationException("A resposta da API retornou um objeto nulo.");
+                }
+
+                return tarefa;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"❗ Erro em Buscar Tarefa: {ex.Message}");
+                throw;
+            }
+        }
+        
+        public async Task<TarefaCadastro> CadastraTarefa(TarefaCadastro tarefaCadastro)
+        {
+            try
+            {
+                var response = await _http.PostAsJsonAsync($"{GlobalSettings.Instance.Tarefa}/cadastrar", tarefaCadastro);
+                
+                if (!response.IsSuccessStatusCode)
+                {
+                    var erro = await response.Content.ReadAsByteArrayAsync();
+                    var utf8String = Encoding.UTF8.GetString(erro);
+                    var json = JsonDocument.Parse(utf8String);
+                    string mensagemErro = "Erro desconhecido.";
+
+                    if (json.RootElement.TryGetProperty("mensagem", out var mensagem))
+                    {
+                        if (mensagem.ValueKind == JsonValueKind.Array)
+                        {
+                            var mensagemArray = mensagem.EnumerateArray().Select(x => x.GetString());
+                            mensagemErro = string.Join("\n", mensagemArray);
+                        }
+                        else
+                        {
+                            mensagemErro = mensagem.GetString() ?? mensagemErro;
+                        }
+                    }
+                    else if (json.RootElement.TryGetProperty("error", out var error))
+                    {
+                        mensagemErro = error.GetString() ?? mensagemErro;
+                    }
+                    throw new Exception(mensagemErro);
+                }
+                var tarefa = await response.Content.ReadFromJsonAsync<TarefaCadastro>();
+
+                if (tarefa == null)
+                {
+                    throw new InvalidOperationException("A resposta da API retornou um objeto nulo.");
+                }
+                return tarefa;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"❗ Erro em CadastraTarefa: {ex.Message}");
+                throw;
+            }
+        }
+        
+        public async Task<TarefaCadastro> EditarTarefa(int tarefaId, TarefaCadastro tarefaEditada)
+        {
+            try
+            {
+                var response = await _http.PutAsJsonAsync($"{GlobalSettings.Instance.Tarefa}/editar/{tarefaId}", tarefaEditada);
+                if (!response.IsSuccessStatusCode)
+                {
+                    var erro = await response.Content.ReadAsByteArrayAsync();
+                    var utf8String = Encoding.UTF8.GetString(erro);
+
+                    string mensagemErro = "Erro ao editar tarefa.";
+                    try
+                    {
+                        var json = JsonDocument.Parse(utf8String);
+                        if (json.RootElement.TryGetProperty("mensagem", out var mensagem))
+                        {
+                            mensagemErro = mensagem.GetString() ?? mensagemErro;
+                        }
+                        else if (json.RootElement.TryGetProperty("error", out var error))
+                        {
+                            mensagemErro = error.GetString() ?? mensagemErro;
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Erro desconhecido!");
+                    }
+
+                    throw new Exception(mensagemErro);
+                }
+
+                var tarefaAtualizada = await response.Content.ReadFromJsonAsync<TarefaCadastro>();
+
+                if (tarefaAtualizada == null)
+                {
+                    throw new InvalidOperationException("A resposta da API retornou um objeto nulo");
+                }
+                return tarefaAtualizada;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro em editar tarefa: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<TarefaLista> AlterarStatusTarefa(int idTarefa, object payload)
+        {
+            try
+            {
+                var response = await _http.PutAsJsonAsync($"{GlobalSettings.Instance.Tarefa}/editar/status/{idTarefa}", payload);
+                if (!response.IsSuccessStatusCode)
+                {
+                    var erro = await response.Content.ReadAsByteArrayAsync();
+                    var utf8String = Encoding.UTF8.GetString(erro);
+
+                    string mensagemErro = "Erro ao editar status da tarefa.";
+                    try
+                    {
+                        var json = JsonDocument.Parse(utf8String);
+                        if (json.RootElement.TryGetProperty("mensagem", out var mensagem))
+                        {
+                            mensagemErro = mensagem.GetString() ?? mensagemErro;
+                        }
+                        else if (json.RootElement.TryGetProperty("error", out var error))
+                        {
+                            mensagemErro = error.GetString() ?? mensagemErro;
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Erro desconhecido!");
+                    }
+
+                    throw new Exception(mensagemErro);
+                }
+                var tarefaAtualizada = await response.Content.ReadFromJsonAsync<TarefaLista>();
+
+                if (tarefaAtualizada == null)
+                {
+                    throw new InvalidOperationException("A resposta da API retornou um objeto nulo");
+                }
+
+                return tarefaAtualizada;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro em editar tarefa: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<bool> DeletarTarefa(int tarefaId)
+        {
+            try
+            {
+                var response = await _http.DeleteAsync($"{GlobalSettings.Instance.Tarefa}/{tarefaId}");
+                if (!response.IsSuccessStatusCode)
+                {
+                    var erro = await response.Content.ReadAsByteArrayAsync();
+                    var utf8String = Encoding.UTF8.GetString(erro);
+
+                    string mensagemErro = "Erro ao excluir tarefa.";
+                    try
+                    {
+                        var json = JsonDocument.Parse(utf8String);
+                        if (json.RootElement.TryGetProperty("error", out var error))
+                        {
+                            mensagemErro = error.GetString() ?? mensagemErro;
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Erro desconhecido!");
+                    }
+
+                    throw new Exception(mensagemErro);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"❗ Erro em DeletarTarefa: {ex.Message}");
+                return false;
+            }
         }
     }
 }
