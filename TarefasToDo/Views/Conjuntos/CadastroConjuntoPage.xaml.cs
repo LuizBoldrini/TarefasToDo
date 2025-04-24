16efe601b38bc1232e2f1f62ea83379a3409b8a8
LuizBoldrini <luizboldriniej@gmail.com>
2025-04-24 17:40:15 -0400

    Implementando Estilo Entry padrão e refatorado formatação do titulo

--- TarefasToDo/Views/Conjuntos/CadastroConjuntoPage.xaml.cs
+++ TarefasToDo/Views/Conjuntos/CadastroConjuntoPage.xaml.cs
@@ -12,42 +12,51 @@ public partial class CadastroConjuntoPage : ContentPage
 		InitializeComponent();
 	}
 
-	private async void CadastrarButton_Clicked(object sender, EventArgs e)
-	{
-		var nome = NomeEntry.Text;
-		var descricao = DescricaoEntry.Text;
-		var _usuario = AppState.UsuarioAtual;
+    private async void CadastrarButton_Clicked(object sender, EventArgs e)
+    {
+        var nome = NomeEntry.Text;
+        var descricao = DescricaoEntry.Text;
+        var _usuario = AppState.UsuarioAtual;
 
-		if (string.IsNullOrWhiteSpace(nome) || string.IsNullOrWhiteSpace(descricao))
-		{
-			await DisplayAlert("Alerta", "Preencha todos os campos", "Ok");
-			return;
-		}
+        if (string.IsNullOrWhiteSpace(nome) || string.IsNullOrWhiteSpace(descricao))
+        {
+            await DisplayAlert("Alerta", "Preencha todos os campos", "Ok");
+            return;
+        }
 
+        if (_usuario == null)
+        {
+            await DisplayAlert("Alerta", "Usuário não identificado, realize o login novamente!", "Ir para login");
+            await Shell.Current.GoToAsync("///LoginPage");
+            return;
+        }
 
-		if (_usuario == null)
-		{
-			await DisplayAlert("Alerta", "Usuário não identificado, realize o login novamente!", "Ir para login");
-			await Shell.Current.GoToAsync("///LoginPage");
-		}
+        try
+        {
+            var novoConjuto = new ConjuntoCadastro
+            {
+                Nome = nome,
+                Descricao = descricao,
+                UsuarioId = _usuario.Id
+            };
 
-		try
-		{
-			var novoConjuto = new ConjuntoCadastro
-			{
-				Nome = nome,
-				Descricao = descricao,
-				UsuarioId = _usuario.Id
-			};
+            var conjunto = await _api.CadastraConjunto(novoConjuto);
+            await DisplayAlert("Sucesso", "Grupo criado com sucesso", "Ok");
+            await Shell.Current.GoToAsync("///ConjuntoPage");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro no Cadastro de conjunto:{ex.Message}");
+            await DisplayAlert("Aviso", ex.Message, "OK");
+        }
+    }
+    private async void VoltarButton_Clicked(object sender, EventArgs e)
+    {
+        bool confirmar = await DisplayAlert("Aviso", "Deseja cancelar o cadastro?", "Sim", "Não");
 
-			var conjunto = await _api.CadastraConjunto(novoConjuto);
-			await DisplayAlert("Sucesso", "Grupo criado com sucesso", "Ok");
-			await Shell.Current.GoToAsync("///ConjuntoPage");
-		}
-		catch (Exception ex)
-		{
-			Console.WriteLine($"Erro no Cadastro de conjunto:{ex.Message}");
-			await DisplayAlert("Aviso", ex.Message, "OK");
-		}
-	}
+        if (confirmar)
+        {
+            await Shell.Current.GoToAsync("///ConjuntoPage", true);
+        }
+    }
 }
--- TarefasToDo/Views/Conjuntos/EditarConjuntoPage.xaml.cs
+++ TarefasToDo/Views/Conjuntos/EditarConjuntoPage.xaml.cs
@@ -6,55 +6,64 @@ namespace TarefasToDo.Views.Conjuntos;
 
 public partial class EditarConjuntoPage : ContentPage
 {
-	private readonly ServiceAPI _api = new();
-	private ConjuntoLista _conjuntoAtual;
+    private readonly ServiceAPI _api = new();
+    private ConjuntoLista _conjuntoAtual = null!;
 
-	public EditarConjuntoPage()
-	{
-		InitializeComponent();
-	}
+    public EditarConjuntoPage()
+    {
+        InitializeComponent();
+    }
 
     protected override void OnAppearing()
     {
         base.OnAppearing();
 
-		_conjuntoAtual = AppState.ConjuntoSelecionado;
+        if (AppState.ConjuntoSelecionado != null)
+        {
+            _conjuntoAtual = AppState.ConjuntoSelecionado;
 
-		if(_conjuntoAtual != null )
-		{
-			NomeEntry.Text = _conjuntoAtual.Nome;
-			DescricaoEntry.Text = _conjuntoAtual.Descricao;
-		}
-		else
-		{
-			DisplayAlert("Erro", "Nenhum conjunto selecionado para edição.", "Ok");
-			Shell.Current.GoToAsync("///ConjuntoPage");
-		}
+            NomeEntry.Text = _conjuntoAtual.Nome;
+            DescricaoEntry.Text = _conjuntoAtual.Descricao;
+        }
+        else
+        {
+            DisplayAlert("Erro", "Nenhum conjunto selecionado para edição.", "Ok");
+            Shell.Current.GoToAsync("///ConjuntoPage");
+        }
     }
 
-	private async void Salvar_Clicked(object sender, EventArgs e)
-	{
-		if(string.IsNullOrWhiteSpace(NomeEntry.Text))
-		{
-			await DisplayAlert("Aviso", "Preencha os campos", "Ok");
-			return;
-		}
+    private async void Salvar_Clicked(object sender, EventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(NomeEntry.Text))
+        {
+            await DisplayAlert("Aviso", "Preencha os campos", "Ok");
+            return;
+        }
 
-		var conjuntoEditado = new ConjuntoCadastro
-		{
-			Nome = NomeEntry.Text,
-			Descricao = DescricaoEntry.Text,
-		};
+        var conjuntoEditado = new ConjuntoCadastro
+        {
+            Nome = NomeEntry.Text,
+            Descricao = DescricaoEntry.Text,
+        };
 
-		try
-		{
-			await _api.EditarConjunto(_conjuntoAtual.Id, conjuntoEditado);
-			await DisplayAlert("Sucesso", "Grupo atualziado com sucesso!", "Ok");
-			await Shell.Current.GoToAsync("///ConjuntoPage");
-		}
-		catch (Exception ex)
-		{
-			await DisplayAlert("Erro", $"Erro ao atualizar grupo: {ex.Message}", "Ok");
-		}
-	}
+        try
+        {
+            await _api.EditarConjunto(_conjuntoAtual.Id, conjuntoEditado);
+            await DisplayAlert("Sucesso", "Grupo atualziado com sucesso!", "Ok");
+            await Shell.Current.GoToAsync("///ConjuntoPage");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", $"Erro ao atualizar grupo: {ex.Message}", "Ok");
+        }
+    }
+    private async void VoltarButton_Clicked(object sender, EventArgs e)
+    {
+        bool confirmar = await DisplayAlert("Aviso", "Deseja cancelar o cadastro?", "Sim", "Não");
+
+        if (confirmar)
+        {
+            await Shell.Current.GoToAsync("///ConjuntoPage", true);
+        }
+    }
 }
