ce6034af29ca9a17acbb98598ef64202a993d5c7
LuizBoldrini <luizboldriniej@gmail.com>
2025-04-25 15:45:08 -0400

    Alterado classe para controle do evento para alteração de status na pagina estatica.

--- TarefasToDo/Models/Tarefas/TarefaLista.cs
+++ TarefasToDo/Models/Tarefas/TarefaLista.cs
@@ -1,15 +1,112 @@
-﻿
+﻿using System.ComponentModel;
+
 namespace TarefasToDo.Models.Tarefas
 {
-    public class TarefaLista
+    public class TarefaLista : INotifyPropertyChanged
     {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Descricao { get; set; }
-        public string Status { get; set; }
-        public int ConjuntoId { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public event PropertyChangedEventHandler? PropertyChanged;
 
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private int _id;
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged(nameof(Id));
+                }
+            }
+        }
+
+        private string _nome = string.Empty;
+        public string Nome
+        {
+            get => _nome;
+            set
+            {
+                if (_nome != value)
+                {
+                    _nome = value;
+                    OnPropertyChanged(nameof(Nome));
+                }
+            }
+        }
+
+        private string _descricao = string.Empty;
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (_descricao != value)
+                {
+                    _descricao = value;
+                    OnPropertyChanged(nameof(Descricao));
+                }
+            }
+        }
+
+        private string _status = string.Empty; 
+        public string Status
+        {
+            get => _status;
+            set
+            {
+                if (_status != value)
+                {
+                    _status = value;
+                    OnPropertyChanged(nameof(Status));
+                }
+            }
+        }
+
+        private int _conjuntoId;
+        public int ConjuntoId
+        {
+            get => _conjuntoId;
+            set
+            {
+                if (_conjuntoId != value)
+                {
+                    _conjuntoId = value;
+                    OnPropertyChanged(nameof(ConjuntoId));
+                }
+            }
+        }
+
+        private DateTime _createdAt;
+        public DateTime CreatedAt
+        {
+            get => _createdAt;
+            set
+            {
+                if (_createdAt != value)
+                {
+                    _createdAt = value;
+                    OnPropertyChanged(nameof(CreatedAt));
+                }
+            }
+        }
+
+        private DateTime _updatedAt;
+        public DateTime UpdatedAt
+        {
+            get => _updatedAt;
+            set
+            {
+                if (_updatedAt != value)
+                {
+                    _updatedAt = value;
+                    OnPropertyChanged(nameof(UpdatedAt));
+                }
+            }
+        }
     }
 }
